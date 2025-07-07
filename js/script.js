eea68a1450f5e636b4fceb2000a18f4d908b8033
MungujakisaChrispus <popularchris8@gmail.com>
2025-07-07 21:55:31 +0300

    button responsiveness still lucking

--- js/script.js
+++ js/script.js
@@ -17,10 +17,25 @@ ScrollReveal({
     duration: 2000,
     delay: 200,
 });
-ScrollReveal().reveal('.home-content, heading', { origin: 'top' });
-ScrollReveal().reveal('.home-image, .service-container,.portfolio-box,.contact form', { origin: 'bottom' });
-ScrollReveal().reveal('.home-content h1, about-image', { origin: 'left' })
-ScrollReveal().reveal('.home-content p, home-content', { origin: 'right' });
+// ScrollReveal().reveal('.home-image, .service-container,.portfolio-box,.contact form', { origin: 'bottom' });
+// ScrollReveal().reveal('.home-content h1, about-image', { origin: 'left' })
+// ScrollReveal().reveal('.home-content p, home-content', { origin: 'right' });
+// ScrollReveal().reveal('.adventure-btn', { origin: 'left' })
+// ScrollReveal().reveal('.adventure-btn1', { origin: 'right' })
+// ScrollReveal().reveal('.home-content p, home-content', { origin: 'right' });
+// ScrollReveal().reveal('.starter-content', { origin: 'top' });
+// ScrollReveal().reveal('.about-content', { origin: 'top' });
+// ScrollReveal().reveal('.ceo-heading', { origin: 'top' });
+// ScrollReveal().reveal('.ceop', { origin: 'right' });
+// ScrollReveal().reveal('.ceoimg', { origin: 'left' });
+// ScrollReveal().reveal('.about-content', '.home-content', { origin: 'top' });
+// ScrollReveal().reveal('.services', { origin: 'bottom' });
+// ScrollReveal().reveal('.spanheading', { origin: 'top' });
+// ScrollReveal().reveal('.spanp', { origin: 'left' });
+// ScrollReveal().reveal('.destination-plan', { origin: 'right' });
+// ScrollReveal().reveal('.footer-box', { origin: 'bottom' });
+
+
 
 
 /*==================== typed js ====================*/
