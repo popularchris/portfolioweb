10a70efdb6bc848d18fdf16c1987acf8e30c0b18
MungujakisaChrispus <popularchris8@gmail.com>
2025-07-09 13:14:13 +0300

    home drop down and scroll reveal

--- js/script.js
+++ js/script.js
@@ -18,33 +18,33 @@ ScrollReveal({
     delay: 200,
 });
 ScrollReveal().reveal('.home-image, .service-container,.portfolio-box,.contact form', { origin: 'bottom' });
-ScrollReveal().reveal('.home-content h1, about-image', { origin: 'left' })
-ScrollReveal().reveal('.home-content p, home-content', { origin: 'right' });
-ScrollReveal().reveal('.adventure-btn', { origin: 'left' })
-ScrollReveal().reveal('.adventure-btn1', { origin: 'right' })
-ScrollReveal().reveal('.home-content p, home-content', { origin: 'right' });
+// ScrollReveal().reveal('.home-content h1, about-image', { origin: 'left' })
+// ScrollReveal().reveal('.home-content p, home-content', { origin: 'right' });
+// ScrollReveal().reveal('.adventure-btn', { origin: 'left' })
+// ScrollReveal().reveal('.adventure-btn1', { origin: 'right' })
+// ScrollReveal().reveal('.home-content p, home-content', { origin: 'right' });
 ScrollReveal().reveal('.starter-content', { origin: 'top' });
 ScrollReveal().reveal('.about-content', { origin: 'top' });
 ScrollReveal().reveal('.ceo-heading', { origin: 'top' });
-ScrollReveal().reveal('.ceop', { origin: 'right' });
-ScrollReveal().reveal('.ceoimg', { origin: 'left' });
+// ScrollReveal().reveal('.ceop', { origin: 'right' });
+// ScrollReveal().reveal('.ceoimg', { origin: 'left' });
 ScrollReveal().reveal('.about-content', '.home-content', { origin: 'top' });
 ScrollReveal().reveal('.services', { origin: 'bottom' });
 ScrollReveal().reveal('.spanheading', { origin: 'top' });
-ScrollReveal().reveal('.spanp', { origin: 'left' });
-ScrollReveal().reveal('.destination-plan', { origin: 'right' });
+// ScrollReveal().reveal('.spanp', { origin: 'left' });
+// ScrollReveal().reveal('.destination-plan', { origin: 'right' });
 ScrollReveal().reveal('.footer-box', { origin: 'bottom' });
-ScrollReveal().reveal('.headline');
+
 
 const sec = document.querySelectorAll("section");
 const navLi = document.querySelectorAll("li");
 window.addEventListener("scroll", () => {
     let current = "";
-    sec.forEach((section) => {
-        const sectionTop = section.offsetTop;
-        const sectionHeight = section.clientHeight;
+    sec.forEach((sec) => {
+        const sectionTop = sec.offsetTop;
+        const sectionHeight = sec.clientHeight;
         if (pageYOffset >= sectionTop - sectionHeight / 3) {
-            current = section.getAttribute("id");
+            current = sec.getAttribute("id");
         }
     });
 
